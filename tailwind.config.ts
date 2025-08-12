a0c9e7233d507f3af93fe0b4d2572f7df218ec5f
Suzy <suzy.nakayama@gmail.com>
2025-08-12 16:23:15 -0400

    added images and icons from old portfolio

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+// import typography from '@tailwindcss/typography';
+
+// const config: Config = {
+//   darkMode: 'class',
+//   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+//   // theme: {
+//   //   extend: {
+//   //     fontFamily: {
+//   //       head: ['var(--font-head)', 'sans-serif'],
+//   //       body: ['var(--font-body)', 'sans-serif'],
+//   //     },
+//   //     colors: {
+//   //       'violet-light': '#EDE0FF',
+//   //       'violet-dark': '#4B0082',
+//   //       'rose-light': '#F8B7E3',
+//   //     },
+//   //   },
+//   // },
+//   plugins: [typography],
+// };
+
+// export default config;
